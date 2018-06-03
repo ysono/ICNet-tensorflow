071a7c14c750b2dbe1ee9fa7ba9597017f63b1eb
Yoshiaki Sono <yoshiaki.sono@gmail.com>
2018-06-03 16:27:37 -0700

    use a separately trained model for center of the image

--- bn_infer_video_frozen_2models_centercrop.py
+++ bn_infer_video_frozen_2models_centercrop.py
@@ -12,11 +12,14 @@ import sys
 
 NOTEGO_MASK = np.invert(np.load('udacity-lyft-challenge-ego-mask-bool.npy'))
 
-def extract_2cls(preds_3cls):
+ROAD_THRESHOLD = 0.3
+CAR_THRESHOLD = 0.3
+
+def extract_2cls(logits_2positiveclasses):
     preds_2cls = []
-    for pred_3cls in preds_3cls:
-        road = (pred_3cls == 1) & NOTEGO_MASK
-        nonego_car = (pred_3cls == 2) & NOTEGO_MASK
+    for logits in logits_2positiveclasses:
+        road = (logits[..., 0] > ROAD_THRESHOLD) & NOTEGO_MASK
+        nonego_car = (logits[..., 1] > CAR_THRESHOLD) & NOTEGO_MASK
         preds_2cls.append((nonego_car, road))
     return preds_2cls
 
@@ -39,12 +42,12 @@ def main(frozen_model_path, frozen_model_path_centercrop, video_path):
     graph_def = tf.GraphDef()
     with tf.gfile.FastGFile(frozen_model_path, "rb") as f:
         graph_def.ParseFromString(f.read())
-    tf.import_graph_def(graph_def)
-    inpt, outpt = [
-        tf.get_default_graph().get_tensor_by_name(n) for n in [
-            'import/inputs_600_800:0', 'import/output_sparse:0'
-        ]
-    ]
+    
+    inpt, outpt = tf.import_graph_def(graph_def,
+        return_elements=['inputs_600_800:0', 'output_2positiveclasses:0'],
+        name='')
+
+    logits = tf.sigmoid(outpt)
 
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
@@ -55,14 +58,14 @@ def main(frozen_model_path, frozen_model_path_centercrop, video_path):
     
     answer = {}
     frame_idx = 0 # Frame numbering starts at 1
-    batch_size = 112
+    batch_size = 136
 
     for batch_i in range(0, frames.shape[0], batch_size):
         frames_batch = frames[batch_i : batch_i+batch_size]
 
-        preds_3cls = sess.run(outpt, feed_dict={inpt: frames_batch})
+        logits_2positiveclasses = sess.run(logits, feed_dict={inpt: frames_batch})
 
-        preds_2cls = extract_2cls(preds_3cls)
+        preds_2cls = extract_2cls(logits_2positiveclasses)
 
         for nonego_car, road in preds_2cls:
             frame_idx += 1
@@ -76,24 +79,21 @@ def main(frozen_model_path, frozen_model_path_centercrop, video_path):
     graph_def = tf.GraphDef()
     with tf.gfile.FastGFile(frozen_model_path_centercrop, "rb") as f:
         graph_def.ParseFromString(f.read())
-    tf.import_graph_def(graph_def)
-    inpt, outpt = [
-        tf.get_default_graph().get_tensor_by_name(n) for n in [
-            'import/inputs_192_256:0', 'import/output_sparse:0'
-        ]
-    ]
+    
+    inpt, outpt = tf.import_graph_def(graph_def,
+        return_elements=['inputs_192_256:0', 'output_sparse:0'],
+        name='')
 
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
     sess = tf.Session(config=config)
 
-    center_h, center_w = (192, 256)
     center_y0, center_x0 = (int((608 - 192)/ 2), int((800 - 256) / 2))
     center_y1 = center_y0 + 192
     center_x1 = center_x0 + 256
 
     frame_idx = 0 # Frame numbering starts at 1
-    batch_size = 112
+    batch_size = 136
     for batch_i in range(0, frames.shape[0], batch_size):
         frames_batch = frames[batch_i : batch_i+batch_size]
         frames_batch = frames_batch[:, center_y0:center_y1, center_x0:center_x1, :]
@@ -105,7 +105,7 @@ def main(frozen_model_path, frozen_model_path_centercrop, video_path):
         for nonego_car, road in preds_2cls:
             frame_idx += 1
 
-            answer[frame_idx][0][center_y0:center_y1, center_x0:center_x1] = nonego_car
+            # answer[frame_idx][0][center_y0:center_y1, center_x0:center_x1] = nonego_car
             # answer[frame_idx][0][center_y0:center_y1, center_x0:center_x1] = answer[frame_idx][0][center_y0:center_y1, center_x0:center_x1] | nonego_car
             # answer[frame_idx][0][center_y0:center_y1, center_x0:center_x1] = answer[frame_idx][0][center_y0:center_y1, center_x0:center_x1] & nonego_car
 
